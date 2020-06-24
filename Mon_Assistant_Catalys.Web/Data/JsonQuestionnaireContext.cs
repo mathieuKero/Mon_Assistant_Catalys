75747a75559b98bfdde73a666c2762ec3ea544d1
mathieuKero <m.keromnes@iia-laval.fr>
2020-06-24 21:21:14 +0200

    Mise à jour classe de données

--- Mon_Assistant_Catalys.Web/Data/JsonQuestionnaireContext.cs
+++ Mon_Assistant_Catalys.Web/Data/JsonQuestionnaireContext.cs
@@ -1,14 +1,56 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mon_Assistant_Catalys.Web.Models
 {
     /// <summary>
-    /// Cette classe va lire les données du questionnaire, C'est une sorte de classe d'accès aux données comme on le ferait avec la DAO
+    ///     Cette classe va lire les données du questionnaire. C'est une sorte de classe d'accès aux données comme on le ferait avec la DAO
     /// </summary>
     public class JsonQuestionnaireContext
     {
+        #region Fields
+
+        /// <summary>
+        ///     Questionnaire.
+        /// </summary>
+        private Questionnaire _Questionnaire { get; set; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public JsonQuestionnaireContext() => this._Questionnaire = new Questionnaire();
+
+        #endregion
+
+        public void LoadData()
+        {
+            string path = @"C:\temp\Config.json";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                
+
+                //Questionnaire questionnaire = new Questionnaire();
+
+                string json = reader.ReadToEnd();
+
+                JObject obj = JObject.Parse(json);
+                JArray array = (JArray)obj["Search"];
+                Questionnaire content = array.ToObject<Questionnaire>();
+
+                //questionnaire.Add(content);
+
+            
+
+            }
+
+        }
+
     }
 }
---
+++ Mon_Assistant_Catalys.Web/Models/Questionnaire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mon_Assistant_Catalys.Web.Models
+{
+    /// <summary>
+    /// COntiens les informations relatives au questionnaire (Liste des questionReponses etc.)
+    /// </summary>
+    public class Questionnaire
+    {
+        /// <summary>
+        ///     Initialise une nouvelle instance de la classe <see cref="Questionnaire"/>.
+        /// </summary>
+        public Questionnaire()
+        {
+
+        }
+
+    }
+}
