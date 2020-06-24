2780e0eb0237bb77ed8c3fcd23b3175ec55f57e1
mathieuKero <m.keromnes@iia-laval.fr>
2020-06-24 22:18:27 +0200

    Correction fichier json & initialisation class Questionnaire

--- Mon_Assistant_Catalys.Web/Models/Questionnaire.cs
+++ Mon_Assistant_Catalys.Web/Models/Questionnaire.cs
@@ -10,6 +10,17 @@ namespace Mon_Assistant_Catalys.Web.Models
     /// </summary>
     public class Questionnaire
     {
+
+        #region Properties
+
+        private string Nom { get; set; }
+
+        private int IdQuestionnaire { get; set; }
+
+        public System.Collections.Generic.Dictionary<string, Questionnaire> QuestionReponse { get; set; }
+
+        #endregion
+
         /// <summary>
         ///     Initialise une nouvelle instance de la classe <see cref="Questionnaire"/>.
         /// </summary>
