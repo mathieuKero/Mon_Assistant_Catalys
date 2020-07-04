5740007eaa446f09d93b5c888a8fd2958bfebcf7
chockella <jorisguerrier@gmail.com>
2020-07-04 13:38:27 +0200

    Modif traitement fichier Json

--- Mon_Assistant_Catalys.Web/Services/QuestionnaireService.cs
+++ Mon_Assistant_Catalys.Web/Services/QuestionnaireService.cs
@@ -1,8 +1,8 @@
 ﻿namespace Mon_Assistant_Catalys.Web.Services
 {
     /// <summary>
-    /// Liaison vers les données et appel des fonction. 
-    /// C'est une couche intermédiaire entre les models / views vers le dossier data.
+    ///     Liaison vers les données et appel des fonction. 
+    ///     C'est une couche intermédiaire entre les models / views vers le dossier data.
     /// </summary>
     public class QuestionnaireService
     {
